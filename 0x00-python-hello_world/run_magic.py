e10e2823dd3dd42066d294a9fa2fed129c8a62cc
Axcis.380 <anas.baidouri@gmail.com>
2023-09-04 14:56:36 -0700

    hey how hua

--- 0x00-python-hello_world/run_magic.py
+++ 0x00-python-hello_world/run_magic.py
@@ -1,16 +1,12 @@
-import importlib.util
+# Define the bytecode instructions as a multiline string
+bytecode_instructions = """
+  3           0 LOAD_CONST               1 (98)
+              3 LOAD_FAST                0 (a)
+              6 LOAD_FAST                1 (b)
+              9 BINARY_POWER
+             10 BINARY_ADD
+             11 RETURN_VALUE
+"""
 
-# Define the module name (with hyphen)
-module_name = "102-magic_calculation"
-
-# Use importlib to create a module object from the file
-module_spec = importlib.util.spec_from_file_location(module_name, module_name + ".py")
-magic_calculation_module = importlib.util.module_from_spec(module_spec)
-module_spec.loader.exec_module(magic_calculation_module)
-
-# Call the function from the dynamically imported module
-result = magic_calculation_module.magic_calculation(3, 5)
-
-# Print the result
-print("Result:", result)
-
+# Print the bytecode instructions
+print(bytecode_instructions)
