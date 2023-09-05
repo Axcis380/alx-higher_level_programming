734588a4367eb3f8a80cbfb152708f76c74e1bd8
Axcis.380 <anas.baidouri@gmail.com>
2023-09-05 08:11:37 -0700

    hello

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-# Author -ELBAIDOURI Anas
+# Author: ELBAIDOURI Anas
 
 i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(c - i)), end="")
     i = 32 if i == 0 else 0
+
+print()  
