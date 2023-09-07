5f7fb9980bddf7acd4012cab260a893f661b17c8
Axcis.380 <anas.baidouri@gmail.com>
2023-09-07 04:02:13 -0700

    maj

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
-    from magic_calculation_102 import add, sub
+    """Performs magical calculations."""
+    from magic_calculation_102 import add, sub  # Make sure the module and functions exist.
 
     if a < b:
-        c = add(a, b)
+        c = add(a, b)  # Using the add function
         for i in range(4, 6):
-            c = add(c, i)
-        return (c)
+            c = add(c, i)  # Using the add function again
+        return c  # Return the computed value
 
     else:
-        return(sub(a, b))
+        return sub(a, b)  # Using the sub function for the "else" branch
