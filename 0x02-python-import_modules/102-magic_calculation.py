b10f95f4de6a7b2ec3ab0560b9c0065d4638dfac
Axcis.380 <anas.baidouri@gmail.com>
2023-09-07 04:07:02 -0700

    maj

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,14 @@
-def magic_calculation(a, b):
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return c
-    else:
-        return sub(a, b)
+#!/usr/bin/python3
 
-import dis
-dis.dis(magic_calculation)
+def magic_calculation(a, b):
+    """Performs magical calculations."""
+    from magic_calculation_102 import add, sub  # Make sure the module and functions exist.
+
+    if a < b:
+        c = add(a, b)  # Using the add function
+        for i in range(4, 6):
+            c = add(c, i)  # Using the add function again
+        return c  # Return the computed value
+
+    else:
+        return sub(a, b)  # Using the sub function for the "else" branch
