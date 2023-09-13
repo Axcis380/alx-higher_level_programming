7b005ee8c5a97e26e87e69095bb47108eb769232
Axcis.380 <anas.baidouri@gmail.com>
2023-09-13 14:48:54 -0700

    hey update

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -2,11 +2,12 @@
 #include <Python.h>
 
 /**
- * print_python_bytes - Prints bytes information
+ * print_python_bytes_info - Prints information about Python bytes objects
  *
- * @p: Python Object
- * Return: no return
+ * @p: Python bytes object
+ * Return: No return value
  */
+
 void print_python_bytes(PyObject *p)
 {
 	char *string;
@@ -42,11 +43,12 @@ void print_python_bytes(PyObject *p)
 }
 
 /**
- * print_python_list - Prints list information
+ * print_python_list_info - Prints information about Python list objects
  *
- * @p: Python Object
- * Return: no return
+ * @p: Python list object
+ * Return: No return value
  */
+
 void print_python_list(PyObject *p)
 {
 	long int size, i;
