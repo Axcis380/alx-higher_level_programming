f5e9b8739426a157f5f825083c6da1f9f02bf86f
Axcis.380 <anas.baidouri@gmail.com>
2023-09-25 07:39:48 -0700

    fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    """Print an inter with "{:d}".format().Args:value(int): The intger to print.
+    """Print an integer with "{:d}".format().
 
-    Returns:If a TypeError or ValueError occurs False.Otherwise True.
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True.
     """
     try:
         print("{:d}".format(value))
--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,11 +4,17 @@ import sys
 
 
 def safe_print_integer_err(value):
-    """Prints an integer with"{:d}".format().
- If a ValueError messge is cught, a corresponng message is printed to standard error. Args value (int): The integer to print.
+    """Prints an integer with "{:d}".format().
 
-    Returns: If a TypeError or ValueError occurs False.
-        Otherwise True.
+    If a ValueError message is caught, a corresponding
+    message is printed to standard error.
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True.
     """
     try:
         print("{:d}".format(value))
